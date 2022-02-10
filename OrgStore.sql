b4f26157bf75c3064fa8e932c01a4568f01856c7
MarydeM <maryevelyndemarigny@gmail.com>
2022-02-09 22:16:18 -0600

    Added Consumer Table

--- OrgStore.sql
+++ OrgStore.sql
@@ -2,3 +2,17 @@ create schema if not exists OrgStoreSchema;
 
 use OrgStoreSchema;
 
+-- ENTITIES
+
+-- There will be a row with the same TransNum for every Item purchased in order to allow us 
+-- to not have a multi-valued column
+create table if not exists Consumer(
+		TransNum int NOT NULL,
+        Item varchar(12) NOT NULL,
+        primary key (TransNum)
+        );
+
+
+-- ----------------------------------------------------------------------------------------
+-- RELATIONS
+
