3b83e1f31b2d13fc2fc0a1637a79dbd9f1923441
brenaeks <71780820+brenaeks@users.noreply.github.com>
2022-02-11 21:29:40 -0600

    Update OrgStore.sql
    Tables for Product, Supplier, Store, and Employee

--- OrgStore.sql
+++ OrgStore.sql
@@ -11,6 +11,39 @@ create table if not exists Consumer(
         Item varchar(12) NOT NULL,
         primary key (TransNum)
         );
+	
+create table PRODUCT ( 
+	PID varchar(12) not null, 
+	SID varchar(4) not null,
+	Price char,
+	Quantity not null,
+	FOREIGN KEY (SID) REFERENCES STORE (SID)
+primary key(PID)
+);
+
+
+create table SUPPLIER ( 
+	SupID varchar(4) not null, 
+	Location char not null, 
+	ProductionRate char
+primary key(SupID)
+);
+
+
+create table STORE ( 
+	SID varchar(4) not null,
+	S_Name var,
+	Location var,
+primary key(SID)
+);
+
+
+create table EMPLOYEE ( 
+	SSN varchar(9) not null, 
+	Address char, 
+	Name char not null,
+primary key(SSN)
+);
 
 
 -- ----------------------------------------------------------------------------------------
